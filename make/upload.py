b2fb247869974ff4afa7aedf41f8dce190e5b803
Jonas Schmidt <mail@schmidt-jonas.de>
2023-05-03 13:35:00 +0200

    remove unnecessary print

--- make/upload.py
+++ make/upload.py
@@ -350,7 +350,6 @@ def main():
                         help='upload a contest?')
 
     args = parser.parse_args()
-    print(args)
 
     if args.contest != (args.problem_zip is None and args.validator_zip is None):
         exit_error('Specify either contest or problem data')
